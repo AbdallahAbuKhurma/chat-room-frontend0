9e5e883ade43a2d00de01d2905f288f6214d3941
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-07-18 00:03:48 +0300

    hotfix

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5000/';
+const SERVER_URL = process.env.SERVER_URL || 'https://abdallah-ibrahim-chat-room.herokuapp.com';
 // const SERVER_URL = process.env.SERVER_URL || 'https://abdallah-chat-room-backend.herokuapp.com/';
 let socket;
 
@@ -33,6 +33,7 @@ const Chat = ({ location }) => {
         alert(error);
       }
     });
+     // eslint-disable-next-line
   }, [SERVER_URL, location.search]);
   
   useEffect(() => {
